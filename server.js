32ce9a2350f938f6ddb4f3e99b5dae4a813fe88f
Parth Desai <pdesai@fc-md.umd.edu>
2016-01-15 14:42:51 -0500

    Inital commit

---
+++ server.js
@@ -0,0 +1,24 @@
+
+//requires
+var express = require('express');
+
+//variables
+var app = express();
+
+//routes
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.listen(3000);
+
+/*
+console.log("work?");
+
+var http = require('http');
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('hello http');
+});
+server.listen(8080);
+*/
